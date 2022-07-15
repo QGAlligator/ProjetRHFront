a656dcdbdb0441c7024f767710dd2301cd662cd6
QGAlligator <94532575+QGAlligator@users.noreply.github.com>
2022-07-15 13:07:06 +0200

    Première Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => 
+    import('./interview/interview-routing.module')
+    .then(mod => mod.InterviewRoutingModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
