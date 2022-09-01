b8a0c545a9e17b942b7271ee790206c13ca819a5
QGAlligator <94532575+QGAlligator@users.noreply.github.com>
2022-09-01 14:25:21 +0200

    Début du pré remplissage du formulaire

--- src/app/interviewform/interviewform.component.ts
+++ src/app/interviewform/interviewform.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { map, Observable, Subject, takeUntil, tap } from 'rxjs';
+import { CandidatsService } from '../services/candidats.service';
 
 @Component({
   selector: 'prh-interviewform',
@@ -13,13 +14,22 @@ export class InterviewformComponent implements OnInit, OnDestroy {
 
   public interviewId: number | null = null;
 
+  public candidatName: string | null = null;
+  public candidatFirstName: string | null = null;
+  public candidatStatut: string | null = null;
+  public candidatDesc: string | null = null;
+  public candidatDate: Date | null = null;
+
   form?: FormGroup;
 
   public get nameControl() {
     return this.form?.get('name');
   }
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private candidatsService: CandidatsService
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap
@@ -36,12 +46,38 @@ export class InterviewformComponent implements OnInit, OnDestroy {
       )
       .subscribe();
 
+    if (this.interviewId) {
+      this.getCandidatsById$(this.interviewId);
+    }
+
     this.initForm$()
       .pipe(
         tap((value) => console.log(this.form)),
         takeUntil(this.destroy$)
       )
       .subscribe();
+  }
+
+  public getCandidatsById$(__id: number) {
+    this.candidatsService
+      .getCandidatsById$(__id)
+      .pipe(
+        map((response: any) => {
+          this.candidatName = response.name;
+          this.candidatFirstName = response.firstname;
+          this.candidatStatut = response.statut;
+          this.candidatDesc = response.desc;
+          this.candidatDate = response.date;
+          console.log(response);
+          console.log(this.candidatDate);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
+        complete: () => {
+          console.log('Jai fini');
+        },
+      });
   }
 
   public getControlStatus(controlName: string, errorName: string[]): boolean {
@@ -66,21 +102,39 @@ export class InterviewformComponent implements OnInit, OnDestroy {
   }
 
   private initForm$(): Observable<any> {
-    this.form = new FormGroup({
-      name: new FormControl('', [
-        Validators.required,
-        Validators.maxLength(30),
-        Validators.minLength(2),
-      ]),
-      firstName: new FormControl('', [
-        Validators.required,
-        Validators.maxLength(30),
-        Validators.minLength(2),
-      ]),
-      statut: new FormControl('', [Validators.required]),
-      desc: new FormControl('', [Validators.maxLength(4000)]),
-      date: new FormControl('', [Validators.required]),
-    });
+    if (this.interviewId) {
+      this.form = new FormGroup({
+        name: new FormControl(this.candidatName, [
+          Validators.required,
+          Validators.maxLength(30),
+          Validators.minLength(2),
+        ]),
+        firstName: new FormControl(this.candidatFirstName, [
+          Validators.required,
+          Validators.maxLength(30),
+          Validators.minLength(2),
+        ]),
+        statut: new FormControl('', [Validators.required]),
+        desc: new FormControl(this.candidatDesc, [Validators.maxLength(4000)]),
+        date: new FormControl('', [Validators.required]),
+      });
+    } else {
+      this.form = new FormGroup({
+        name: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(30),
+          Validators.minLength(2),
+        ]),
+        firstName: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(30),
+          Validators.minLength(2),
+        ]),
+        statut: new FormControl('', [Validators.required]),
+        desc: new FormControl('', [Validators.maxLength(4000)]),
+        date: new FormControl('', [Validators.required]),
+      });
+    }
     return this.form.valueChanges;
   }
 
--- src/app/services/candidats.service.ts
+++ src/app/services/candidats.service.ts
@@ -7,61 +7,6 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class CandidatsService {
-  // public candidats$: Candidat[] = [
-  //   {
-  //     id: 1,
-  //     name: 'Doe',
-  //     firstname: 'John',
-  //     statut: 'En Cours',
-  //     desc: 'A compléter',
-  //     date: new Date('09-26-2022'),
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Toto',
-  //     firstname: 'Toto',
-  //     statut: 'Réponse Positive',
-  //     desc: 'Sérieux, aimable et curieux',
-  //     date: new Date('01-14-2021'),
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'EscrotDuBar',
-  //     firstname: 'Pablo',
-  //     statut: 'Réponse Négative',
-  //     desc: "Arrive en retard et n'est pas très poli",
-  //     date: new Date('12-08-2020'),
-  //   },
-  // ];
-
-  // public meta$: Meta[] = [
-  //   {
-  //     limit: 20,
-  //     total: this.candidats$.length,
-  //     offset: 0,
-  //   },
-  // ];
-
-  // constructor(protected httpclient: HttpClient) {}
-
-  // public getCandidat$(): Observable<Candidat[]> {
-  //   //return this.httpclient.get('/candidats')
-  //   return of(this.candidats$).pipe(
-  //     map((candidats: any) =>
-  //       candidats.map((candidat: any): Candidat => candidat as Candidat)
-  //     )
-  //   );
-  // }
-
-  // public getMeta$(): Observable<Meta[]> {
-  //   //return this.httpclient.get('/candidats')
-  //   return of(this.meta$).pipe(
-  //     map((meta: any) =>
-  //       meta.map((candidat: any): Candidat => candidat as Candidat)
-  //     )
-  //   );
-  // }
-
   public candidats: Candidat[] = [
     {
       id: 1,
@@ -105,6 +50,16 @@ export class CandidatsService {
   public getCandidats$(): Observable<ApiResponse> {
     return of(this.ApiResponse$).pipe(
       map((response: any): ApiResponse => response as ApiResponse)
+    );
+  }
+
+  public getCandidatsById$(_id: number): Observable<Candidat> {
+    let index = 0;
+    while (_id != this.candidats[index].id) {
+      index++;
+    }
+    return of(this.candidats[index]).pipe(
+      map((response: any): Candidat => response as Candidat)
     );
   }
 }
